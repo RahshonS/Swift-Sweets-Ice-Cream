d31f9400b25565eb324bbdd60e6afb8eadf29321
Rahshon <tuf70447@temple.edu>
2021-08-02 19:02:16 -0700

    Image added to main page of the site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import icecream from "./pictures/ice-cream2.jpg"
 
+//Header component 
+//{props.businessName} is a JSX expression 
+function Header(props){
+  return(
+    <header>
+      <h1>{props.businessName} Ice Cream Shop</h1>
+    </header>
+  )
+}
+
+function Main(props){
+  return (
+    <section>
+      <p>Mix and Match to make your dream treat with over {props.toppingCount} Toppings and counting</p>
+
+      <img src={icecream} height={150} alt="a cute cartoon ice cream cone"/>
+      
+      <ul>
+        {props.ourToppings.map((topping) => (
+          <li key={topping.id}>{topping.title}</li>))}
+      </ul>
+      
+    </section>
+  )
+}
+
+function Footer(props){
+  return(
+    <p>As of {props.currentYear} we've catered 765 events!</p>
+  )
+}
+
+//display this list in our main component by mapping over it
+//then makking the data available to the component
+const toppings = [
+  "Rainbow Sprinkles",
+  "Chocoalate Sprinkles",
+  "Brownie Chunks",
+];
+
+//This is building the object b4 we pass it to a component as a property 
+const toppingObjects = toppings.map((topping, i) => ({id: i, title: topping}));console.log(toppingObjects);
+
+//console log each topping
+//This helps with mapping 
+//but will reside in main
+
+
+//App component
+//<Header /> is the tag we created that allows us to call / render to header component
+//businessName is the name of the property we wish to pass in to our header component 
+//here is where we pass whatever we want to our components
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header businessName="Swift Sweets"/>          
+        <Main toppingCount = "forty-four" ourToppings={toppingObjects} />
+        <Footer currentYear={new Date().getFullYear()}/>
     </div>
   );
 }
